d477986badf89f30bd19b481b020da4a3696da0a
Dan <dan.seeruttun@gmail.com>
2021-05-01 19:00:36 +0200

    fix accents

--- Genealogy/src/main/java/Genealogy/GUI/CustomOutputStream.java
+++ Genealogy/src/main/java/Genealogy/GUI/CustomOutputStream.java
@@ -1,8 +1,11 @@
 package Genealogy.GUI;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.swing.*;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Objects;
 
 /**
  * This class extends from OutputStream to redirect output to a JTextArrea
@@ -24,13 +27,24 @@ public class CustomOutputStream extends OutputStream {
         String value = Character.toString(c);
         buffer.append(value);
         if (value.equals("\n")) {
-            textArea.append(buffer.toString());
+            textArea.append(StringUtils.trim(buffer.toString()) + value);
             buffer.delete(0, buffer.length());
             // scrolls the text area to the end of data
             textArea.update(textArea.getGraphics());
             textArea.invalidate();
             textArea.validate();
             textArea.repaint();
+        }
+    }
+
+    @Override
+    public void write(byte in[], int off, int len) throws IOException {
+        String value = StringUtils.stripAccents(new String(in));
+        byte[] b = value.getBytes();
+        Objects.checkFromIndexSize(off, len, b.length);
+        // len == 0 condition implicitly handled by loop bounds
+        for (int i = 0; i < len; i++) {
+            write(b[off + i]);
         }
     }
 }
